47e7edd400ec12ef468f5944c294a7495b2f7f4c
sakthimuthuselvan <sakthimsd531@gmail.com>
2022-08-12 17:07:00 +0530

    hello

---
+++ script/javascript.js
@@ -0,0 +1,13 @@
+// for(var i=0; i<=10; i++){
+// console.log(i);
+// }
+
+// while loop
+var a=10;
+while(a<50){
+    console.log(a);
+}
+console.log("arithmetic operaters")
+console.log("add:"+1+2);
+console.log("sub:"+1-5);
+console.log("div:"+1-10)
