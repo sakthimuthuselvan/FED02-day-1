c81802fa9ecf42de9a7e5f7ccb3f1ba33fd612eb
sakthimuthuselvan <sakthimsd531@gmail.com>
2022-08-17 16:58:34 +0530

    javascript

--- script/javascript.js
+++ script/javascript.js
@@ -2,12 +2,60 @@
 // console.log(i);
 // }
 
-// while loop
-var a=10;
-while(a<50){
-    console.log(a);
+// // while loop
+// var a=10;
+// b=13;
+// c=a+=b;
+// console.log(c);
+
+
+// const first="mr.";
+
+
+// let second="sakthi";
+// let center=first+"sai"+second;
+// console.log(th);
+// alert(center )
+
+
+// let a =10;
+// {
+//     let b =30;
+//     // console .log(b);
+// }
+// console.log(a);
+
+
+
+// let name1;
+// {
+//     name1 = "spark";
+//      name2="sakthi";
+// }
+// console .log(name1+" "+name2);
+
+// let num1=10;
+// let num2=30;
+// if(num1==num2){
+//     console.log("these values are equal");
+// }else if(num1>num2){
+//     console .log("this value is grater than 30");
+
+// }else{
+//     console.log("30 grater than 10");
+// 
+
+//object 
+
+let person={
+    nam:"sakthi",
+    age:21,
+    isname:true,
+    address:"anna nagar ,tenkasi,",
+    brother:{
+        sibling1:"sakthi",
+        sibling2:"thambu"
+    }
 }
-console.log("arithmetic operaters")
-console.log("add:"+1+2);
-console.log("sub:"+1-5);
-console.log("div:"+1-10)
+console.log(person.brother.sibling1);
+console.log(person);
